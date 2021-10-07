182ff0c5a2724eab4839123603f180954ed8228c
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-08 03:42:24 +0530

    integrated sawo

--- pages/signin.js
+++ pages/signin.js
@@ -1,20 +1,28 @@
 // import Sawo from "sawo";
+import SawoLogin from "sawo-react";
 import { useState } from "react";
+import styles from "../styles/SignIn.module.css";
+import Footer from "../components/Footer/Footer";
 const signin = () => {
   const [userPayload, setUserPayload] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const sawoConfig = {
+    onSuccess: sawoLoginCallback, //required
+    identifierType: "phone_number_sms", //required, must be one of: 'email', 'phone_number_sms',
+    apiKey: process.env.NEXT_PUBLIC_SAWO_API_KEY, // required, get it from sawo dev.sawolabs.com,
+    containerHeight: "100%", // the login container height, default is 230px
+  };
+  function sawoLoginCallback(payload) {
+    console.log(payload);
+  }
   return (
-    <div>
-      {!isLoggedIn ? (
-        <div
-          id="sawo-container"
-          style={{ height: "300px", width: "300px" }}
-        ></div>
-      ) : (
-        "Logged In!"
-      )}
-    </div>
+    <>
+      <div className={styles.formContainer}>
+        <SawoLogin config={sawoConfig} />
+      </div>
+      <Footer/>
+    </>
   );
 };
 
