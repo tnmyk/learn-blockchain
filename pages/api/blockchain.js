485bd7546ac418df6f61b2af129acf924766697d
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-08 20:54:11 +0530

    redeploy, bugs

--- pages/api/blockchain.js
+++ pages/api/blockchain.js
@@ -1,9 +1,9 @@
 import { db } from "../../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
 
 
 export default async function handler(req, res) {
-  let querySnapshot = await getDocs(collection(db, "blockchain"));
+  let querySnapshot = await getDocs(query(collection(db, "blockchain"),orderBy("timestamp")));
   let blockchain = [];
   querySnapshot.forEach((doc) => {
     blockchain.push(doc.data());
