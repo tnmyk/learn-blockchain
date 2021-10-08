b7c85b3af1996b37cd7296d42707470ebdcaeec7
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-08 17:22:17 +0530

    fixed api

--- pages/api/blockchain.js
+++ pages/api/blockchain.js
@@ -1,15 +1,12 @@
 import { db } from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 
-const getBlockchain = async () => {
-  let querySnapshot = await getDocs(collection(db, "blockchain"));
-  let blockchain = []
-  querySnapshot.forEach((doc)=>{
-    blockchain.push(doc.data())
-  })
-  return blockchain;
-};
+
 export default async function handler(req, res) {
-  const blockchain = await getBlockchain()
-  res.status(200).json(blockchain);
+  let querySnapshot = await getDocs(collection(db, "blockchain"));
+  let blockchain = [];
+  querySnapshot.forEach((doc) => {
+    blockchain.push(doc.data());
+  });
+  res.json(blockchain);
 }
