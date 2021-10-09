fd58afd7772b77f1faa895654de05aaa9b03ab7b
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-09 19:53:09 +0530

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
